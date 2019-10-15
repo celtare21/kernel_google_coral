e603a557377fc66bafcc12bc0aeec965137f3291
Jilai Wang <jilaiw@codeaurora.org>
2019-10-15 13:48:46 -0400

    msm: npu: Avoid mutex dead lock if turning on clocks failed
    This change is to fix an issue which will casue mutex dead lock
    if turning on clocks failed.
    
    Change-Id: Ib9e26315e4da1fda18739e319bccec819315c3c5
    Signed-off-by: Jilai Wang <jilaiw@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/media/platform/msm/npu/npu_dev.c
+++ drivers/media/platform/msm/npu/npu_dev.c
@@ -630,18 +630,19 @@ int npu_enable_core_power(struct npu_device *npu_dev)
 	if (!pwr->pwr_vote_num) {
 		ret = npu_enable_regulators(npu_dev);
 		if (ret)
-			return ret;
+			goto fail;
 
 		ret = npu_enable_core_clocks(npu_dev);
 		if (ret) {
 			npu_disable_regulators(npu_dev);
 			pwr->pwr_vote_num = 0;
-			return ret;
+			goto fail;
 		}
 		pwr->cur_dcvs_activity = DCVS_MODE_MAX;
 		npu_resume_devbw(npu_dev);
 	}
 	pwr->pwr_vote_num++;
+fail:
 	mutex_unlock(&npu_dev->dev_lock);
 
 	return ret;
