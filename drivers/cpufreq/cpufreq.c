f45160a704017856927727cf956c13e4ad74d459
celtare21 <celtare21@gmail.com>
2020-01-13 15:14:52 +0000

    cpufreq: Limit big and prime cluster max frequency
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2232,6 +2232,12 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	if (ret)
 		return ret;
 
+	if (cpumask_test_cpu(new_policy->cpu, cpu_perf_mask))
+		cpufreq_verify_within_limits(new_policy, new_policy->min, 2323200);
+
+	if (cpumask_test_cpu(new_policy->cpu, cpu_perfp_mask))
+		cpufreq_verify_within_limits(new_policy, new_policy->min, 2649600);
+
 	/* adjust if necessary - all reasons */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_ADJUST, new_policy);
