dd54ee74942eed2fa150c6ee3f63f88460cfe09c
celtare21 <celtare21@gmail.com>
2020-03-03 10:01:36 +0000

    kernel: printk: Force devkmsg_log to DEVKMSG_LOG_MASK_OFF
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -114,14 +114,8 @@ static int __control_devkmsg(char *str)
 	if (!str)
 		return -EINVAL;
 
-	if (!strncmp(str, "on", 2) || !strncmp(str, "ratelimit", 9)) {
-		devkmsg_log = DEVKMSG_LOG_MASK_DEFAULT;
-		return 9;
-	} else if (!strncmp(str, "off", 3)) {
-		devkmsg_log = DEVKMSG_LOG_MASK_OFF;
-		return 3;
-	}
-	return -EINVAL;
+	devkmsg_log = DEVKMSG_LOG_MASK_OFF;
+	return 3;
 }
 
 static int __init control_devkmsg(char *str)
