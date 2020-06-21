7d0aa47146e4cd9cf30ea1e086bc8f3bb463185f
celtare21 <celtare21@gmail.com>
2020-06-21 22:16:04 +0200

    sched: fair: Make sync_boost local to select_task_rq_fair()
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8411,11 +8411,12 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	int want_affine = 0;
 	int want_energy = 0;
 	int sync = wake_flags & WF_SYNC;
+	bool sync_boost = sync && cpu >= cpu_rq(cpu)->rd->mid_cap_orig_cpu;
 
 	if (energy_aware()) {
 		rcu_read_lock();
 		new_cpu = find_energy_efficient_cpu(energy_sd, p,
-						cpu, prev_cpu, sync);
+						cpu, prev_cpu, sync, sync_boost);
 		rcu_read_unlock();
 		return new_cpu;
 	}
@@ -8511,7 +8512,6 @@ pick_cpu:
 			    cpu_rq(cpu)->rd->mid_cap_orig_cpu != -1 ?
 			     cpu_rq(cpu)->rd->mid_cap_orig_cpu :
 			     cpu_rq(cpu)->rd->max_cap_orig_cpu;
-			bool sync_boost = sync && cpu >= high_cap_cpu;
 
 			new_cpu = find_energy_efficient_cpu(energy_sd, p, cpu,
 						    prev_cpu, sync, sync_boost);
