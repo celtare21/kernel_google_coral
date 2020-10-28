b3a6f6f1d59fb21de1686e32b9499b46792dc7e5
celtare21 <celtare21@gmail.com>
2020-10-28 20:02:46 +0000

    sched: Read and write prefer_high_cap
     * Just in case the boost hal isn't happy.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -53,6 +53,9 @@ struct schedtune {
 	/* Hint to bias scheduling of tasks on that SchedTune CGroup
 	 * towards idle CPUs */
 	int prefer_idle;
+
+	/* Stub */
+	bool prefer_high_cap;
 };
 
 static inline struct schedtune *css_st(struct cgroup_subsys_state *css)
@@ -89,6 +92,7 @@ root_schedtune = {
 	.colocate_update_disabled = false,
 #endif
 	.prefer_idle = 0,
+	.prefer_high_cap = false,
 };
 
 /*
@@ -588,12 +592,18 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 static u64 prefer_high_cap_read(struct cgroup_subsys_state *css,
 				struct cftype *cft)
 {
-	return 0;
+	struct schedtune *st = css_st(css);
+
+	return st->prefer_high_cap;
 }
 
 static int prefer_high_cap_write(struct cgroup_subsys_state *css,
 				 struct cftype *cft, u64 prefer_high_cap)
 {
+	struct schedtune *st = css_st(css);
+
+	st->prefer_high_cap = !!prefer_high_cap;
+
 	return 0;
 }
 
