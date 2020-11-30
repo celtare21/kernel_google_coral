4c8ef4391bd620c4cafafe7fdd8d2f5a9aebf62a
Sultan Alsawaf <sultan@kerneltoast.com>
2020-11-29 19:59:51 -0800

    google_charger: Eliminate floating point constant for exp_mw
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/power/supply/google/google_charger.c
+++ drivers/power/supply/google/google_charger.c
@@ -2144,8 +2144,8 @@ static int pps_policy(struct chg_drv *chg_drv, int fv_uv, int cc_max)
 	}
 
 	/* TODO: should we compensate for the round down here? */
-	exp_mw = (unsigned long)vbatt * (unsigned long)cc_max * 1.1 /
-		 1000000000;
+	exp_mw = (unsigned long)vbatt * (unsigned long)cc_max * 11 /
+		 10000000000;
 
 	if (ibatt >= 0)
 		return 0;
