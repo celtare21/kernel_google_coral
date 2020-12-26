df83b4050d2d31bf94336f443d5985087f2237c0
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 20:52:03 -0800

    msm: ipa: Fix compile errors when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/platform/msm/ipa/ipa_clients/ipa_mhi_client.c
+++ drivers/platform/msm/ipa/ipa_clients/ipa_mhi_client.c
@@ -175,7 +175,6 @@ struct ipa_mhi_client_ctx {
 static struct ipa_mhi_client_ctx *ipa_mhi_client_ctx;
 static DEFINE_MUTEX(mhi_client_general_mutex);
 
-
 static char *ipa_mhi_channel_state_str[] = {
 	__stringify(IPA_HW_MHI_CHANNEL_STATE_DISABLE),
 	__stringify(IPA_HW_MHI_CHANNEL_STATE_ENABLE),
@@ -519,6 +518,10 @@ fail:
 	debugfs_remove_recursive(dent);
 }
 
+static void ipa_mhi_debugfs_destroy(void)
+{
+	debugfs_remove_recursive(dent);
+}
 #else
 static void ipa_mhi_debugfs_init(void) {}
 static void ipa_mhi_debugfs_destroy(void) {}
@@ -2581,13 +2584,6 @@ int ipa_mhi_destroy_all_channels(void)
 	IPA_MHI_FUNC_EXIT();
 	return 0;
 }
-
-#ifdef CONFIG_DEBUG_FS
-static void ipa_mhi_debugfs_destroy(void)
-{
-	debugfs_remove_recursive(dent);
-}
-#endif
 
 static void ipa_mhi_delete_rm_resources(void)
 {
