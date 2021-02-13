a54817e51a931584651d4813ad0d4688a14cafc0
celtare21 <celtare21@gmail.com>
2021-02-13 12:24:26 -0500

    [FIXUP] msm: kgsl: Avoid dynamically allocating small command buffers
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -972,7 +972,7 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 	struct kgsl_memobj_node *ib;
 	unsigned int numibs = 0;
 	unsigned int *link;
-	unsigned int link_onstack[SZ_256] __aligned(8);
+	unsigned int link_onstack[SZ_256] __aligned(sizeof(long));
 	unsigned int *cmds;
 	struct kgsl_context *context;
 	struct adreno_context *drawctxt;
@@ -1107,8 +1107,7 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 	if (gpudev->ccu_invalidate)
 		dwords += 4;
 
-	if (likely(dwords <= ARRAY_SIZE(link_onstack))) {
-		memset(link_onstack, 0, dwords * sizeof(unsigned int));
+	if (dwords <= ARRAY_SIZE(link_onstack)) {
 		link = link_onstack;
 	} else {
 		link = kcalloc(dwords, sizeof(unsigned int), GFP_KERNEL);
@@ -1243,7 +1242,7 @@ done:
 	trace_kgsl_issueibcmds(device, context->id, numibs, drawobj->timestamp,
 			drawobj->flags, ret, drawctxt->type);
 
-	if (unlikely(link != link_onstack))
+	if (link != link_onstack)
 		kfree(link);
 	return ret;
 }
