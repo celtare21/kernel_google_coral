d7bf29b4263aa7603ed688d7f4bd438e569a6ce9
celtare21 <celtare21@gmail.com>
2021-07-15 13:10:09 -0400

    Revert "drm/msm: Don't bail out when debugfs creation fails"
    This reverts commit f715235eeb9679aff4b39d343290cb8944e7f29a.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -902,10 +902,14 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 		priv->fbdev = msm_fbdev_init(ddev);
 #endif
 
-	if (!msm_debugfs_late_init(ddev)) {
-		priv->debug_root = debugfs_create_dir("debug",
-						ddev->primary->debugfs_root);
-		sde_dbg_debugfs_register(priv->debug_root);
+	ret = msm_debugfs_late_init(ddev);
+	if (ret)
+		goto fail;
+
+	ret = sde_dbg_debugfs_register(dev);
+	if (ret) {
+		dev_err(dev, "failed to reg sde dbg debugfs: %d\n", ret);
+		goto fail;
 	}
 
 	/* perform subdriver post initialization */
