ffec0caa58bb1a54ef4909510836ad0a40f2ca5f
celtare21 <celtare21@gmail.com>
2021-07-15 13:21:13 -0400

    techpack: audio: Fixup
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- techpack/audio/dsp/codecs/audio_g711mlaw.c
+++ techpack/audio/dsp/codecs/audio_g711mlaw.c
@@ -286,7 +286,6 @@ static int audio_open(struct inode *inode, struct file *file)
 	}
 
 	snprintf(name, sizeof(name), "msm_g711_%04x", audio->ac->session);
-#ifdef CONFIG_DEBUG_FS
 	audio->dentry = config_debugfs_create_file(name, (void *)audio);
 
 	if (IS_ERR_OR_NULL(audio->dentry))
