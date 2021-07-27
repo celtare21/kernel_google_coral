77847b42e93fd1391e03ab4b912d33e84510271f
celtare21 <celtare21@gmail.com>
2021-07-27 14:15:41 -0400

    init: userland_worker: Update
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- init/userland_worker.c
+++ init/userland_worker.c
@@ -244,45 +244,15 @@ static inline int linux_chmod(const char* path, const char* perms)
 	return use_userspace(argv);
 }
 
-static inline void set_tee(void)
+static inline void first_command_retry(void)
 {
 	int ret, retries = 0;
 
 	do {
-		ret = linux_write("ro.build.fingerprint",
-			"google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys",
-			true);
+		ret = linux_write("artemis.prop", "1", false);
 		if (ret)
 			msleep(DELAY);
 	} while (ret && retries++ < 10);
-
-	linux_write("ro.odm.build.fingerprint",
-		"google/coral/coral:S/SPP1.210122.020.A3/7145137:user/release-keys",
-		true);
-
-	linux_write("ro.product.build.fingerprint",
-		"google/coral/coral:S/SPP1.210122.020.A3/7145137:user/release-keys",
-		true);
-
-	linux_write("ro.system.build.fingerprint",
-		"google/coral/coral:S/SPP1.210122.020.A3/7145137:user/release-keys",
-		true);
-
-	linux_write("ro.system_ext.build.fingerprint",
-		"google/coral/coral:S/SPP1.210122.020.A3/7145137:user/release-keys",
-		true);
-
-	linux_write("ro.vendor.build.fingerprint",
-		"google/coral/coral:S/SPP1.210122.020.A3/7145137:user/release-keys",
-		true);
-
-	linux_write("ro.vendor_dlkm.build.fingerprint",
-		"google/coral/coral:S/SPP1.210122.020.A3/7145137:user/release-keys",
-		true);
-
-	linux_write("ro.bootimage.build.fingerprint",
-		"google/coral/coral:S/SPP1.210122.020.A3/7145137:user/release-keys",
-		true);
 }
 
 static void encrypted_work(void)
@@ -290,7 +260,7 @@ static void encrypted_work(void)
 	if (!linux_sh("/system/bin/su"))
 		is_su = true;
 
-	set_tee();
+	first_command_retry();
 
 	linux_write("ro.iorapd.enable", "false", true);
 
